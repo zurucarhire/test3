2c54c30bc8c8e2c7861a4815bbd41fda717d6cd1
jabala <jabala@cellulant.com>
2022-02-17 11:25:10 +0300

    CICD

--- src/main/java/com/cellulant/iprs/configuration/CorsConfig.java
+++ src/main/java/com/cellulant/iprs/configuration/CorsConfig.java
@@ -16,11 +16,10 @@ public class CorsConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200","scb-iprs-web.18.216.147.169.nip.io"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://scb-iprs-web.18.216.147.169.nip.io"));
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-
 }
